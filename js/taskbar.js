bb128b5e3b04c1c6b36b9489095a3289e00cc58c
cuberJ <2113159231@qq.com>
2025-10-25 23:20:36 +0800

    完善论坛内容

--- js/taskbar.js
+++ js/taskbar.js
@@ -5,11 +5,14 @@
   function createTaskbar(){
     var tb=document.createElement('div');
     tb.className='taskbar';
+    var manual = document.body.dataset.pageIndex || '';
+    var pageHtml = manual ? ('<span class="spacer"></span><span class="page-index" aria-label="页面编号">'+manual+'</span>') : '';
     tb.innerHTML = [
       '<a href="user.html" aria-label="用户信息">👤 用户信息</a>',
       '<a href="news.html" aria-label="每日新闻">📰 每日新闻</a>',
       '<a href="supermarket.html" aria-label="超市主页">🏬 超市主页</a>',
-      '<a href="mail.html" class="mail" aria-label="邮件">\n  <span class="none">📭 邮件</span>\n  <span class="has">📬 有未读邮件</span>\n</a>'
+      '<a href="mail.html" class="mail" aria-label="邮件">\n  <span class="none">📭 邮件</span>\n  <span class="has">📬 有未读邮件</span>\n</a>',
+      pageHtml
     ].join('');
     document.body.appendChild(tb);
   }
